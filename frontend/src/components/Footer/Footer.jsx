a0f2a23fecb16b90f60d7ea1175b49838cbe72d8
spaulraj234 <scute8635@gmail.com>
2025-08-26 06:07:57 +0530

    update all

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo} alt="" />
+          <img src={assets.logo } alt="" />
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque
             nostrum iure suscipit maiores non harum incidunt unde magnam
